f1a5b47611f22f3b2fe0b24f1851ffef58e0e2bb
ubi <me@ubidefeo.com>
2020-11-15 20:34:58 +0100

    renamed main Python file

--- Provisioning/Provisioning.ino
+++ Provisioning/Provisioning.ino
@@ -16,44 +16,44 @@ void setup() {
   while (!Serial);
   
   if (!ECCX08.begin()) {
-	Serial.println("No ECCX08 present!");
-	while (1);
+    Serial.println("No ECCX08 present!");
+    while (1);
   }
 
   if (!ECCX08.locked()) {
-	String lockConfirm = promptAndReadLine("Your ECCX08 is unlocked, would you like to lock it (y/N): ");
-	lockConfirm.toLowerCase();
+    String lockConfirm = promptAndReadLine("Your ECCX08 is unlocked, would you like to lock it (y/N): ");
+    lockConfirm.toLowerCase();
 
-	if (lockConfirm != "y") {
-	  Serial.println("That's all folks");
-	  while (1);
-	}
+    if (lockConfirm != "y") {
+      Serial.println("That's all folks");
+      while (1);
+    }
 
-	if (!ECCX08.writeConfiguration(DEFAULT_ECCX08_TLS_CONFIG)) {
-	  Serial.println("Writing ECCX08 configuration failed!");
-	  while (1);
-	}
+    if (!ECCX08.writeConfiguration(DEFAULT_ECCX08_TLS_CONFIG)) {
+      Serial.println("Writing ECCX08 configuration failed!");
+      while (1);
+    }
 
-	if (!ECCX08.lock()) {
-	  Serial.println("Locking ECCX08 configuration failed!");
-	  while (1);
-	}
+    if (!ECCX08.lock()) {
+      Serial.println("Locking ECCX08 configuration failed!");
+      while (1);
+    }
 
-	Serial.println("ECCX08 locked successfully");
-	Serial.println();
+    Serial.println("ECCX08 locked successfully");
+    Serial.println();
   }
 
   String csrConfirm = promptAndReadLine("Would you like to generate a new private key and CSR (y/N): ");
   csrConfirm.toLowerCase();
 
   if (csrConfirm != "y") {
-	Serial.println("That's all folks");
-	while (1);
+    Serial.println("That's all folks");
+    while (1);
   }
 
   if (!ECCX08Cert.beginCSR(keySlot, true)) {
-	Serial.println("Error starting CSR generation!");
-	while (1);
+    Serial.println("Error starting CSR generation!");
+    while (1);
   }
 
   String deviceId = promptAndReadLine("Please enter the device id: ");
@@ -62,8 +62,8 @@ void setup() {
   String csr = ECCX08Cert.endCSR();
 
   if (!csr) {
-	Serial.println("Error generating CSR!");
-	while (1);
+    Serial.println("Error generating CSR!");
+    while (1);
   }
 
   Serial.println("Generated CSR is:");
@@ -90,13 +90,13 @@ void setup() {
   hexStringToBytes(signature, signatureBytes, sizeof(signatureBytes));
 
   if (!ECCX08.writeSlot(deviceIdSlot, deviceIdBytes, sizeof(deviceIdBytes))) {
-	Serial.println("Error storing device id!");
-	while (1);
+    Serial.println("Error storing device id!");
+    while (1);
   }
 
   if (!ECCX08Cert.beginStorage(compressedCertSlot, serialNumberAndAuthorityKeyIdentifierSlot)) {
-	Serial.println("Error starting ECCX08 storage!");
-	while (1);
+    Serial.println("Error starting ECCX08 storage!");
+    while (1);
   }
 
   ECCX08Cert.setSignature(signatureBytes);
@@ -109,13 +109,13 @@ void setup() {
   ECCX08Cert.setExpireYears(expireYears.toInt());
 
   if (!ECCX08Cert.endStorage()) {
-	Serial.println("Error storing ECCX08 compressed cert!");
-	while (1);
+    Serial.println("Error storing ECCX08 compressed cert!");
+    while (1);
   }
 
   if (!ECCX08Cert.beginReconstruction(keySlot, compressedCertSlot, serialNumberAndAuthorityKeyIdentifierSlot)) {
-	Serial.println("Error starting ECCX08 cert reconstruction!");
-	while (1);
+    Serial.println("Error starting ECCX08 cert reconstruction!");
+    while (1);
   }
 
   ECCX08Cert.setIssuerCountryName("US");
@@ -124,12 +124,12 @@ void setup() {
   ECCX08Cert.setIssuerCommonName("Arduino");
 
   if (!ECCX08Cert.endReconstruction()) {
-	Serial.println("Error reconstructing ECCX08 compressed cert!");
-	while (1);
+    Serial.println("Error reconstructing ECCX08 compressed cert!");
+    while (1);
   }
 
   if (!DEBUG) {
-	return;
+    return;
   }
 
   Serial.println("Compressed cert = ");
@@ -138,12 +138,12 @@ void setup() {
   int certLength = ECCX08Cert.length();
 
   for (int i = 0; i < certLength; i++) {
-	byte b = certData[i];
+    byte b = certData[i];
 
-	if (b < 16) {
-	  Serial.print('0');
-	}
-	Serial.print(b, HEX);
+    if (b < 16) {
+      Serial.print('0');
+    }
+    Serial.print(b, HEX);
   }
   Serial.println();
 }
@@ -163,17 +163,17 @@ String readLine() {
   String line;
 
   while (1) {
-	if (Serial.available()) {
-	  char c = Serial.read();
+    if (Serial.available()) {
+      char c = Serial.read();
 
-	  if (c == '\r') {
-		// ignore
-	  } else if (c == '\n') {
-		break;
-	  }
+      if (c == '\r') {
+        // ignore
+      } else if (c == '\n') {
+        break;
+      }
 
-	  line += c;
-	}
+      line += c;
+    }
   }
 
   line.trim();
@@ -187,12 +187,12 @@ void hexStringToBytes(String& in, byte out[], int length) {
   int outLength = 0;
 
   for (int i = 0; i < inLength && outLength < length; i += 2) {
-	char highChar = in[i];
-	char lowChar = in[i + 1];
+    char highChar = in[i];
+    char lowChar = in[i + 1];
 
-	byte highByte = (highChar <= '9') ? (highChar - '0') : (highChar + 10 - 'A');
-	byte lowByte = (lowChar <= '9') ? (lowChar - '0') : (lowChar + 10 - 'A');
+    byte highByte = (highChar <= '9') ? (highChar - '0') : (highChar + 10 - 'A');
+    byte lowByte = (lowChar <= '9') ? (lowChar - '0') : (lowChar + 10 - 'A');
 
-	out[outLength++] = (highByte << 4) | (lowByte & 0xF);
+    out[outLength++] = (highByte << 4) | (lowByte & 0xF);
   }
 }
